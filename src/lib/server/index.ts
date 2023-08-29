aeb4b8bd03e579c695f8d67438a3b5a43e6fd650
TheHadiAhmadi <thehadiahmadi@gmail.com>
2023-08-29 14:19:11 +0430

    update

--- src/lib/server/index.ts
+++ src/lib/server/index.ts
@@ -1,9 +1,9 @@
-import { DATABASE_HOST, DATABASE_USER, DATABASE_PASSWORD, DATABASE_DB, DATABASE_URI } from '$env/static/private'
 import { connect, type ConnectType } from '@ulibs/db'
 
 export const db: ReturnType<ConnectType> = connect({
-    client: 'pg',
-    connection: DATABASE_URI
+    // client: 'pg',
+    filename: './app.db'
+    // connection: DATABASE_URI
     // host: DATABASE_HOST,
     // user: DATABASE_USER,
     // password: DATABASE_PASSWORD,
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -21,25 +21,25 @@
 <Layout>
     <svelte:fragment slot="sidebar-items">
         <SidebarItem title="Tables" icon="database">
-            <SidebarItem title="Users" icon="users" href="/data/users"/>
-            <SidebarItem title="Blogs" icon="file-text" href="/data/blogs"/>
-            <SidebarItem title="Events" icon="database" href="/data/events"/>
+            <SidebarItem title="Users" icon="users" href="/admin/data/users"/>
+            <SidebarItem title="Blogs" icon="file-text" href="/admin/data/blogs"/>
+            <SidebarItem title="Events" icon="database" href="/admin/data/events"/>
                 
             <!-- <El d="flex" alignItems=center justifyContent="between" slot="title">
                 Data 
             </El> -->
             <El mt=1 ps="2">
-                <Button href="/data/add" w="100" ><Icon name="plus" /> Add Table</Button>
+                <Button href="/admin/data/add" w="100" ><Icon name="plus" /> Add Table</Button>
             </El>
 
         </SidebarItem>
         <SidebarItem title="Pages" icon="file">
-            <SidebarItem href="/pages/home"  icon="home" title="Home"/>
-            <SidebarItem href="/pages/about-us"  icon="file" title="About Us"/>
-            <SidebarItem href="/pages/contact-us"  icon="phone" title="Contact Us"/>
+            <SidebarItem href="/admin/pages/home"  icon="home" title="Home"/>
+            <SidebarItem href="/admin/pages/about-us"  icon="file" title="About Us"/>
+            <SidebarItem href="/admin/pages/contact-us"  icon="phone" title="Contact Us"/>
 
             <El mt=1 ps="2">
-                <Button href="/pages/add" w="100" ><Icon name="plus" /> Add Page</Button>
+                <Button href="/admin/pages/add" w="100" ><Icon name="plus" /> Add Page</Button>
             </El>
 
         </SidebarItem>
